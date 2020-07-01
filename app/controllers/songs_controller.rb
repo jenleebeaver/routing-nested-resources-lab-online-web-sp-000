f61aa5c5c434b8efd0fda2a404a49ac412cca529
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-07-01 22:45:03 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -15,9 +15,11 @@ class SongsController < ApplicationController
     if params[:artist_id]
       @artist = Artist.find_by(id: params[:artist_id])
       @song = @artist.songs.find_by(id: params[:id])
+      if @song.nil?
+        redirect_to artist_songs_path(@artist), alert: "Song not found."
     else
       flash[:alert] = "Artist not found."
-      redirect_to artist
+
   end
 
   def new
